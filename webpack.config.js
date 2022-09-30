43d8b12c804c9c9a63e43004185817d4c2454f76
alibaysarov <112414241+alibaysarov@users.noreply.github.com>
2022-10-01 02:49:12 +0300

    Delete webpack.config.js

--- webpack.config.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-    mode: 'development',
-    devtool: "source-map",
-    output: {
-        filename: 'bundle.js',
-        sourceMapFilename: "dist/js/bundle.js.map",
-        pathinfo: true
-    }
-}
